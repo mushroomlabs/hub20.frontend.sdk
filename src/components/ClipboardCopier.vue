642eb70d8493aeb58c62340481d35e14debec1b8
Raphael Lullis <raphael@mushroomlabs.com>
2021-10-08 11:22:43 +0200

    (feat) add vue-clipboard to allow copying values to clipboard
    For users that are making transactions via the webbrowser, there are
    lots of values that need to copied-and-pasted. This library helps with
    getting clipboard functionality into vue components

--- src/components/ClipboardCopier.vue
+++ src/components/ClipboardCopier.vue
@@ -1,13 +1,19 @@
 <template>
-  <span class="clipboard-copier">
+  <span class="clipboard-copier" @click="copyToClipboard">
     <slot>{{ value }}</slot>
   </span>
 </template>
 
 <script>
 export default {
+  name: 'clipboard-copier',
   props: {
-    value: [String, Number]
-  }
+    value: [String, Number],
+  },
+  methods: {
+    copyToClipboard() {
+      this.$copyText(this.value).catch(() => console.log('Failed to copy text'))
+    },
+  },
 }
 </script>
