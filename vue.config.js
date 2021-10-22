2fbb510baed25d91d215e9976c62a160bde38183
Raphael Lullis <raphael@mushroomlabs.com>
2021-10-22 02:04:33 +0200

    clean up package.json

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
   chainWebpack: config => {
-    config.resolve.alias.set('@', path.resolve(__dirname, './src'))
+    config.resolve.alias.set('@', path.resolve(__dirname, 'src'))
   },
 }
