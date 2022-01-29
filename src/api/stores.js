15d01ba4c1ee9a5849b0bf0a263d80f162142e8e
Raphael Lullis <raphael@mushroomlabs.com>
2022-01-29 22:04:49 +0100

    (fix) checkout_webhook_url was missing from store payload

--- src/api/stores.js
+++ src/api/stores.js
@@ -3,9 +3,13 @@ import client from './client'
 export default {
   _client: client,
   create(storeData) {
-    const {name, site_url, accepted_token_list} = storeData
-    let payload = {name, site_url, accepted_token_list}
-    return this._client.post('/my/stores', payload)
+    const {name, site_url, accepted_token_list, checkout_webhook_url} = storeData
+    return this._client.post('/my/stores', {
+      name,
+      site_url,
+      accepted_token_list,
+      checkout_webhook_url,
+    })
   },
   getList() {
     return this._client.get('/my/stores')
@@ -14,11 +18,10 @@ export default {
     return this._client.get(`/my/stores/${storeId}`)
   },
   update(storeData) {
-    const {url, name, site_url, accepted_token_list} = storeData
-    let payload = {name, site_url, accepted_token_list}
-    return this._client.put(url, payload)
+    const {url, name, site_url, accepted_token_list, checkout_webhook_url} = storeData
+    return this._client.put(url, {name, site_url, accepted_token_list, checkout_webhook_url})
   },
   remove(storeId) {
     return this._client.delete(`/my/stores/${storeId}`)
-  }
+  },
 }
